494d08490bed62c9834b95aa7cc3cfab81ee2804
anujgithub785101 <166162889+anujgithub785101@users.noreply.github.com>
2024-04-06 00:02:09 +0530

    Add files via upload

---
+++ MyFrame.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+class MyFrame
+{
+	MyFrame()
+	{
+		JFrame f=new JFrame("ankitankitankit");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(700,700);
+		f.setVisible(true);
+	}
+	public static void main(String args[])
+	{
+		new MyFrame();
+		System.out.println("sssss");
+	}
+}
---
+++ Temp2.java
@@ -0,0 +1,14 @@
+package p2;
+import p1.Temp1;
+import static p1.Temp1. *;
+import static java.lang.System. *;
+class Temp2
+{
+	public static void main(String args[])
+	{
+		out.println(x);
+		show();
+		new Temp1().show();
+	}
+	
+}
